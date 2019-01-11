8906828ba9188c03483ece08bbfb5456e6ac389f
realchief <stryokka@gmail.com>
2019-01-11 10:59:27 -0800

    updated bgescraping

--- bgescraping/settings.py
+++ bgescraping/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'bgescraping.spiders'
 #USER_AGENT = 'bgescraping (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- bgescraping/spiders/bgescraping.py
+++ bgescraping/spiders/bgescraping.py
@@ -4,24 +4,20 @@ import re
 from selenium import webdriver
 from time import sleep
 import os
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+import requests
 
 
 class BgeSpider(Spider):
     name = "bgescraping"
     start_urls = [
-        'https://eservice.washgas.com/Pages/Login.aspx'
+        'https://secure.bge.com/Pages/Login.aspx'
     ]
-
-    PDF_LINK = 'https://eservice.washgas.com/Standard/BillPayments/Pages/_layouts/BillPayMgmt/ProcessPDFRequest.ashx?acctNum={account_number}&billDate={bill_date}&isInline=true'
     passed_vals = []
 
     def __init__(self, username=None, password=None, download_directory=None, *args, **kwargs):
-        super(WashGasSpider, self).__init__(*args, **kwargs)
+        super(BgeSpider, self).__init__(*args, **kwargs)
         self.user_name = username if username else 'ap@res1.net'
-        self.password = password if password else 'Cy7tW4$RsQ'
+        self.password = password if password else 'paybgenow1!'
         self.download_directory = download_directory if download_directory else 'C:/Users/webguru/Downloads/BGE/'
 
         if not os.path.exists(self.download_directory):
@@ -40,15 +36,14 @@ class BgeSpider(Spider):
         while True:
             try:
                 user_email = self.driver.find_element_by_xpath(
-                    '//form//input[contains(@name, "txtEmail")]'
-                )
+                    '//div[contains(@class, "exc-form-inner exc-tooltip")]//input[contains(@id, "Username")]')
                 user_email.send_keys(self.user_name)
                 password = self.driver.find_element_by_xpath(
-                    '//form//input[contains(@name,"txtPassword")]'
+                    '//div[contains(@class, "exc-form-group-double")]//input[contains(@id,"Password")]'
                 )
                 password.send_keys(self.password)
                 btn_login = self.driver.find_element_by_xpath(
-                    '//form//input[contains(@id, "btnSignIn")]'
+                    '//button[contains(@processing-button, "Signing In...")]'
                 )
                 btn_login.click()
                 break
@@ -58,7 +53,7 @@ class BgeSpider(Spider):
 
         while True:
             try:
-                self.driver.find_element_by_xpath('//span[@class="menu-item-text" and contains(text(), "Account")]')
+                self.driver.find_element_by_xpath('//div[@id="callMeBackContainerApp"]')
                 break
             except:
                 sleep(100)
@@ -72,76 +67,71 @@ class BgeSpider(Spider):
 
         while True:
             try:
-                WebDriverWait(opt, 3).until(EC.alert_is_present(), "eservice.washgas.com says")
+                if self.driver.current_url != 'https://secure.bge.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx':
+                    self.driver.get('https://secure.bge.com/MyAccount/MyBillUsage/Pages/Secure/AccountHistory.aspx')
 
-                alert = opt.switch_to.alert
-                alert.accept()
-                print("alert accepted")
-            except TimeoutException:
-                print("no alert")
-            try:
-                if self.driver.current_url != 'https://eservice.washgas.com/Standard/BillPayments/Pages/BillingPaymentHistory.aspx':
-                    self.driver.get('https://eservice.washgas.com/Standard/BillPayments/Pages/BillingPaymentHistory.aspx')
+                account_selected = True
+                while account_selected:
+                    options = self.driver.find_elements_by_xpath('//select[@id="filter-statement-type"]//option')
+                    if options:
+                        statement_type = options[1]
+                        statement_type.click()
 
-                val = self.driver.find_element_by_xpath('//select[@class="account-dropdown"]//option[@selected]').get_attribute('value')
-                date_val = self.driver.find_elements_by_xpath(
-                    '//select[@id="ctl00_SPWebPartManager1_g_bed3be53_cfc8_4589_ae53_958f96e4a203_ctl00_ddlBillDate"]//option')
+                    search_button = self.driver.find_elements_by_xpath('//button[@id="filter-apply"]')
+                    if search_button:
+                        search_button[0].click()
+                    else:
+                        print "There is no search button"
 
-                for i in range(0, len(date_val)):
-                    v = date_val[i]
-                    v.click()
-                    sleep(1)
-                    v_value = v.get_attribute('value')
+                    account_number = self.driver.find_elements_by_xpath(
+                        '//p[contains(text(), "Account")]//span[@class="exc-data-neutral"]'
+                    )
+                    account_number = account_number[0].text if account_number else None
 
-                    bill_date = v.get_attribute('value').split(';')[-1]
-                    title = self.driver.find_element_by_xpath('//select[@class="account-dropdown"]//option[@selected]').text
-                    title_list = title.split('  ')
-                    account_infos = filter(None, title_list)
-                    account_name = account_infos[0]
-                    account_number = account_infos[1]
-                    if '{} {} {}'.format(account_number, account_name, bill_date) not in self.logs:
-                        print '--------- downloading ---'
-                        yield self.download_page(account_number, bill_date, account_name)
+                    all_pages_crawled = False
+                    while not all_pages_crawled:
 
-                if val not in self.passed_vals:
-                    self.passed_vals.append(val)
-                options = self.driver.find_elements_by_xpath('//select[@class="account-dropdown"]//option[not(@selected)]')
-                print options
-                for opt in options:
-                    if opt.get_attribute('value') not in self.passed_vals:
-                        opt.click()
-                        sleep(2)
-                        break
-                if len(self.passed_vals) == len(options):
-                    break
+                        rows = self.driver.find_elements_by_xpath(
+                            '//table[@class="table bill-history dataTable no-footer dtr-column collapsed"]//tbody//tr'
+                        )
+
+                        for row in rows:
+                            bill_date_info = row.find_elements_by_xpath('.//td[@class="sorting_1"]')[0].text.split('/')
+                            bill_date = bill_date_info[2] + bill_date_info[0] + bill_date_info[1]
+
+                            pdf_link = row.find_elements_by_xpath('.//td[@class="action-cell"]/a')[0].get_attribute('href')
+                            if '{}-{}'.format(account_number, bill_date) not in self.logs:
+                                print '--------- downloading ---'
+                                yield self.download_page(pdf_link, account_number, bill_date)
+
+                        try:
+                            self.driver.find_elements_by_xpath('//li[@class="paginate_button next"]')[0].click()
+                        except:
+                            all_pages_crawled = True
+
+                    try:
+                        self.driver.find_elements_by_xpath(
+                            '//a[@class="btn btn-primary" and contains(text(), "Change Account")]')[0].click()
+                    except:
+                        account_selected = False
+
             except:
                 sleep(2)
                 continue
 
-    def download_page(self, account_number=None, bill_date=None, account_name=None):
+    def download_page(self, pdf_link, account_number=None, bill_date=None):
 
-        file_name = '{} {} {}.pdf'.format(account_number, account_name, bill_date)
-        print "===================================="
-        print file_name
-        print_btn = self.driver.find_elements_by_xpath('//input[@value="View Printable Bill"]')[0]
-        if os.path.exists('C:/Users/webguru/Downloads/PDF_Bill.pdf'):
-            os.remove('C:/Users/webguru/Downloads/PDF_Bill.pdf')
-        if os.path.exists('C:/Users/webguru/Downloads/PDF_Bill (1).pdf'):
-            os.remove('C:/Users/webguru/Downloads/PDF_Bill (1).pdf')
-        print_btn.click()
-        sleep(10)
-        if os.path.exists('C:/Users/webguru/Downloads/PDF_Bill.pdf'):
-            os.rename('C:/Users/webguru/Downloads/PDF_Bill.pdf', 'C:/Users/webguru/Downloads/' + file_name)
-        if os.path.exists('C:/Users/webguru/Downloads/PDF_Bill.pdf.crdownload'):
-            os.rename('C:/Users/webguru/Downloads/PDF_Bill.pdf.crdownload', 'C:/Users/webguru/Downloads/' + file_name)
-        if os.path.exists('C:/Users/webguru/Downloads/PDF_Bill (1).pdf'):
-            os.rename('C:/Users/webguru/Downloads/PDF_Bill (1).pdf', 'C:/Users/webguru/Downloads/' + file_name)
-        sleep(5)
+        raw_pdf = requests.get(pdf_link).content
+        file_name = '{}{}_{}.pdf'.format(self.download_directory, account_number, bill_date)
 
-        self.write_logs('{} {} {}'.format(account_number, account_name, bill_date))
+        with open(file_name, 'wb') as f:
+            f.write(raw_pdf)
+            self.logger.info('{} is downloaded successfully'.format(account_number))
+            f.close()
+        self.write_logs('{}-{}'.format(account_number, bill_date))
         return {
             'file_name': file_name,
-            'account_name': account_name,
+            'file_url': pdf_link,
             'account_number': account_number,
             'bill_date': bill_date
         }
